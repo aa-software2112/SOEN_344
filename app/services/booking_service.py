dd3aeef84d335962b27f1aae1193ecf622058507
laurent <w32.tsim@gmail.com>
2019-02-13 16:50:09 -0500

    Added BookingService

---
+++ app/services/booking_service.py
@@ -0,0 +1,34 @@
+from app.models.booking import Booking
+
+
+class BookingService:
+
+    def __init__(self, db):
+        self.db = db.get_instance(False)
+
+    def write_booking(self, appointment):
+        appointment = appointment.__dict__
+
+        for availability_id in appointment['availability_ids']:
+            insert_stmt = 'INSERT INTO Booking(availability_id, doctor_id, patient_id) ' \
+                          'VALUES (?, ?, ?)'
+            params = (availability_id, appointment['doctor_id'], appointment['patient_id'])
+            self.db.return_none(insert_stmt, params)
+
+    def get_booking(self, booking_id):
+        select_stmt = 'SELECT * FROM Booking WHERE id = ?'
+        params = (booking_id,)
+        result = self.db.return_single(select_stmt, params)
+
+        if result is None:
+            return
+
+        booking = Booking(result['id'], result['availability_id'], result['doctor_id'], result['patient_id'])
+
+        return booking
+
+    def cancel_booking(self, booking_id):
+        delete_stmt = 'DELETE FROM Booking WHERE id = ?'
+        params = (booking_id,)
+        self.db.return_none(delete_stmt, params)
+
