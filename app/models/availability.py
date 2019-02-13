3cf60779b15fc64651aadaccad7bd17c58269cc0
aa-software2112 <a_dreoli@encs.concordia.ca>
2019-02-13 01:39:43 -0500

    Added schedule classes

--- app/models/availability.py
+++ app/models/availability.py
@@ -3,18 +3,53 @@ from app.helpers.date import Date
 
 class Availability:
 
-    def __init__(self, availability_id, doctor_id, start, end, room, year, month, day):
+    SECONDS_IN_HOUR = 60*60
+    SECONDS_IN_MINUTE = 60
+
+    def __init__(self, availability_id, doctor_id, start, end, room, free, year, month, day):
 
         # Note, the id comes from the Availability Table
         self.id = availability_id
         self.doctor_id = doctor_id
         self.start = start
+        self.free = free
         self.end = end
         self.room = room
         self.month = month
         self.day = day
         self.year = year
 
-    def getDate(self):
+    def get_start_time_string(self):
+        return "{}:{} {}".format(int(self.start/(Availability.SECONDS_IN_HOUR)),
+                              int((self.start%Availability.SECONDS_IN_HOUR)/Availability.SECONDS_IN_MINUTE),
+                                 "AM" if self.start / (Availability.SECONDS_IN_HOUR) < 12 else "PM")
 
-        return Date(self.day, self.month, self.year)
+    def get_end_time_string(self):
+        return "{}:{} {}".format(int(self.end/(Availability.SECONDS_IN_HOUR)),
+                              int((self.end%Availability.SECONDS_IN_HOUR)/Availability.SECONDS_IN_MINUTE),
+                                 "AM" if self.end / (Availability.SECONDS_IN_HOUR) < 12 else "PM")
+
+    def get_day(self):
+        return self.day
+
+    def get_date(self):
+
+        return Date( self.year, self.month, self.day)
+
+    def is_free(self):
+
+        return self.free
+
+    def __str__(self):
+
+        return "start {} end {} day {} month {} year {}".format(self.get_start_time_string(),
+                                                                    self.get_end_time_string(),
+                                                                    self.day,
+                                                                    self.month,
+                                                                    self.year)
+
+if __name__ == "__main__":
+
+    a = Availability(5, 6, 12*3600+1380, 12*3600+2580, "GREEN_ROOM", True, 2019, 2, 12)
+    print(a.get_start_time_string())
+    print(a.get_end_time_string())
---
+++ app/models/schedule.py
@@ -0,0 +1,145 @@
+from abc import ABC, abstractmethod
+from calendar import monthrange
+from app.models.availability import Availability
+from app.helpers.date import Date
+
+
+class Schedule(ABC):
+
+    def __init__(self):
+
+        pass
+
+    @abstractmethod
+    def get_schedule_date_string(self):
+        """
+
+        :return: Should return some date string representing
+         the schedule's representation scope (month or day) for front-end display
+        """
+        pass
+
+class DailySchedule(Schedule):
+
+    def __init__(self, list_of_availabilities, date):
+        """
+
+        :param list_of_availabilities: A list of Availability objects corresponding to a single date
+        :param date: The Date object corresponding to the list of availabilites
+        """
+        Schedule.__init__(self)
+
+        self.availabilities = list_of_availabilities
+
+        self.date_of_daily_schedule = date
+
+    def get_schedule_date_string(self):
+
+        return self.date_of_daily_schedule.get_date_string()
+
+    def availability_exists_today(self):
+        """
+        Checks if there is at-least a single availability in this object that is "free"
+        :return: True if there exists a free availability, otherwise False
+        """
+
+        availability_exists = False
+
+        for avail in self.availabilities:
+
+            # The availability is free, so one slot exists on this day
+            if avail.is_free():
+
+                availability_exists = True
+
+                break
+
+        return availability_exists
+
+
+    def display_daily_schedule(self):
+
+        for avail in self.availabilities:
+            print(avail)
+
+class MonthlySchedule(Schedule):
+
+    def __init__(self, list_of_availabilities, date):
+        """
+        This class contains a dictionary of DailySchedules, where each key in the dictionary
+        corresponds to the day of the month, and the value is the DailySchedule itself. This will
+        help render a monthly schedule to the front end
+
+        :param list_of_availabilities: A list of availabilities for an entire month
+        :param date: The date that represents the month - the day attribute need not be set
+        """
+        Schedule.__init__(self)
+
+        self.daily_schedules = {}
+
+        number_of_days_in_month = monthrange(date.get_year(), date.get_month())[1]
+
+        # The minimum and maximum keys of the dictionary of DailySchedules
+        self.min_date_key = 1
+
+        self.max_date_key = number_of_days_in_month
+
+        # Generates all dictionary keys for all days in the month
+        for day in range(self.min_date_key, self.max_date_key + 1):
+            self.daily_schedules[day] = None
+
+        self.date_of_monthly_schedule = date
+
+        self.create_all_daily_schedules(list_of_availabilities)
+
+    def create_all_daily_schedules(self, list_of_availabilities):
+
+        avail_dict = self.daily_schedules.copy()
+
+        # Create a list for each key
+        for k, v in avail_dict.items():
+            avail_dict[k] = []
+
+        # Loop over all availabilities, separating them into lists by date of
+        # availability
+        for avail in list_of_availabilities:
+
+            # Expect integer days
+            avail_dict[int(avail.get_day())].append(avail)
+
+        # Get list of availabilities by day, and create daily schedules of them
+        for day, v in avail_dict.items():
+            self.daily_schedules[day] = DailySchedule(avail_dict[day], Date(self.date_of_monthly_schedule.get_year(),
+                                                                        self.date_of_monthly_schedule.get_month(),day))
+
+    def get_schedule_date_string(self):
+
+        return self.date_of_monthly_schedule.get_date_string()
+
+
+    def display_monthly_schedule(self):
+
+        for day in range(self.min_date_key, self.max_date_key + 1):
+
+            self.daily_schedules[day].display_daily_schedule()
+
+
+if __name__ == "__main__":
+
+    avails = []
+
+    for repetitions in range(1, 12 + 1):
+
+        for day in range(1, 27 + 1):
+
+            for num_avails in range(1, 15 + 1):
+
+                avails.append(Availability(-1, -1, num_avails*3600+(num_avails + repetitions)*20,
+                                           num_avails * 3600 + (num_avails +repetitions )* 20 + 20*60, "RM", True, 2018, 7, day))
+
+
+    m = MonthlySchedule(avails, Date(2018, 7))
+
+    m.display_monthly_schedule()
+
+    print(m.get_schedule_date_string())
