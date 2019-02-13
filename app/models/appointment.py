eab386fc603a1eed8f9f38750f3f4c9707fb6c88
Samuel Gosselin <samuelgosselin1@hotmail.com>
2019-02-13 16:56:19 -0500

    added WalkinAppointment, AnnualAppointment and Cart class

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -3,16 +3,7 @@ class Appointment:
 	When the appointment gets checked out it becomes a Booking object"""
 
 
-	def __init__(self, availability_ids, date, patient_id, doctor_id, patient_name, doctor_name,
-				  start_time, end_time):
-		# by default an appointment is never considered booked
-		self.booked = False
-
-		# the availability_ids corresponds to primary key ids in the
-		# availability table, they are used to validate and reserve the availabilities
-		# and after are used as f_keys (foreign keys) for the booking object
-		self.availability_ids = availability_ids
-
+	def __init__(self, date, patient_id, patient_name, doctor_id, doctor_name, start_time, end_time):
 		# the date of the appointment
 		self.date = date
 
@@ -27,6 +18,29 @@ class Appointment:
 		# these may be passed from the availability objects
 		self.start_time = start_time
 		self.end_time = end_time
+
+class WalkinAppointment(Appointment):
+	def __init__(self, availability_id, date, patient_id, patient_name, doctor_id, doctor_name, start_time, end_time):
+		super().__init__(date, patient_id, patient_name, doctor_id, doctor_name, start_time, end_time)
+		# the availability_id corresponds to primary key id in the
+		# availability table, it used to validate and reserve the availabilities
+		# and after are used as f_keys (foreign keys) for the booking object
+		self.availability_id = availability_id
+
+	def get_availability_id(self):
+		return self.availability_id
+
+class AnnualAppointment(Appointment):
+	def __init__(self, availability_ids, date, patient_id, patient_name, doctor_id, doctor_name, start_time, end_time):
+		super().__init__(date, patient_id, patient_name, doctor_id, doctor_name, start_time, end_time)
+		# the availability_ids is a list of primary keys in the
+		# availability table, they are used to validate and reserve the availabilities
+		# and after are used as f_keys (foreign keys) for the booking object
+		self.availability_ids = availability_ids
+
+	def get_availability_ids(self):
+		return self.availability_ids
+
 
 
 
