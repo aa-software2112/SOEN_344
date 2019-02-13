c13028c8c5d7603e4a9733622439b025172ed58c
aa-software2112 <a_dreoli@encs.concordia.ca>
2019-02-13 00:13:37 -0500

    Added availability class

--- app/models/availability.py
+++ app/models/availability.py
@@ -1,10 +1,20 @@
 import uuid
+from app.helpers.date import Date
 
-class availability:
+class Availability:
 
-    def __init__(self, doctor_id, start, end, room):
-        self.id = uuid.uuid4()
+    def __init__(self, availability_id, doctor_id, start, end, room, year, month, day):
+
+        # Note, the id comes from the Availability Table
+        self.id = availability_id
         self.doctor_id = doctor_id
         self.start = start
         self.end = end
         self.room = room
+        self.month = month
+        self.day = day
+        self.year = year
+
+    def getDate(self):
+
+        return Date(self.day, self.month, self.year)
