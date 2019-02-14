21f52241b45306644448fcfba83adacf435a50a8
lkadian <laurentkadian@gmail.com>
2019-02-14 11:08:30 -0500

    Fixed imports

--- uber_sante/controllers/__init__.py
+++ uber_sante/controllers/__init__.py
@@ -2,6 +2,9 @@ from flask import Blueprint
 
 controllers = Blueprint('controllers', __name__)
 
+from .availability_controller import *
+from .booking_controller import *
+from .patient_controller import *
 
 @controllers.route('/')
 def index():
