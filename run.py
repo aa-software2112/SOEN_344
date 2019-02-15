f3d014229268e9b81b3b3fe8da17a26f3679da00
Krishna Patel <thekrish360@hotmail.com>
2019-02-15 11:27:22 -0500

    Added Vue client

--- run.py
+++ run.py
@@ -2,9 +2,16 @@ from uber_sante import app, db
 from uber_sante.controllers import controllers
 from flask import Flask, request, jsonify
 
+
 @app.route('/')
-def welcome_message(): 
+def welcome_message():
     return 'Hello world'
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
 
 @app.route('/hi', methods=["GET"])
 def test():
@@ -16,7 +23,9 @@ def test():
 
         return jsonify(results), 200
 
-app.register_blueprint(controllers) # always register blueprints after 'app' routes
+
+# always register blueprints after 'app' routes
+app.register_blueprint(controllers)
 
 if __name__ == "__main__":
     app.run(debug=True)
