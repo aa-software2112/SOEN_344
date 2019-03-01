3de3db96f454747f4e00b9560c0440086be4567b
William <william.lussier115@hotmail.com>
2019-03-01 10:59:03 -0500

    Schedule appointment start

--- uber_sante/services/availability_service.py
+++ uber_sante/services/availability_service.py
@@ -1,4 +1,4 @@
-import uber_sante.models.scheduler
+import uber_sante.models
 from uber_sante.utils.dbutil import DBUtil
 from uber_sante.models.availability import Availability
 
@@ -62,14 +62,6 @@ class AvailabilityService:
         return Availability(result['id'], result['doctor_id'], result['start'],
                             result['room'], result['free'], result['year'], result['month'],
                             result['day'], uber_sante.models.scheduler.AppointmentRequestType(result['booking_type']))
-    """
-    def free_availabilities(self, availability_ids):
-
-        for availability_id in availability_ids:
-            update_stmt = 'UPDATE Availability SET free = 1 WHERE id = ?'
-            params = (availability_id,)
-            self.db.write_one(update_stmt, params)
-    """
 
     def free_availability(self, availability_id):
 
