05ecf9e0b3b1baf67e8792086d506cf4f1bee879
laurawheatley <laura.eliza.wheatley@gmail.com>
2019-03-08 16:34:28 -0500

    [#97] Initial Commit for nurse appointment booking, changes to nurse_controller.py

--- uber_sante/controllers/nurse_controller.py
+++ uber_sante/controllers/nurse_controller.py
@@ -40,3 +40,46 @@ def loginNurse():
             resp, nurse_id, cookie_helper.UserTypes.NURSE.value)
 
         return resp, js.ResponseReturnCode.CODE_200.value
+
+@controllers.route('/make-annual-appointment-nurse', methods=['PUT'])
+def make_annual_appointment():
+
+    if request.method == 'PUT':
+
+        #if not cookie_helper.user_is_logged(request):
+        #    return js.create_json(data=None, message="User is not logged", return_code=js.ResponseReturnCode.CODE_400)
+
+        availability_id = int(request.args.get('availability_id'))
+        patient_id = request.args.get('patient_id')
+
+        patient_service.test_and_set_patient_into_cache(patient_id)
+        patient = patient_service.get_patient(patient_id)
+        availability = availability_service.get_availability(availability_id)
+
+        result = patient.make_annual_appointment(availability)
+
+        if result == MakeAnnualStatus.SUCCESS:
+            return js.create_json(data=None, message="Successfully added annual appointment", return_code=js.ResponseReturnCode.CODE_200)
+
+        elif result == MakeAnnualStatus.HAS_ANNUAL_APPOINTMENT:
+            return js.create_json(data=None, message="Patient already has an annual appointment in cart", return_code=js.ResponseReturnCode.CODE_400)
+
+
+@controllers.route('/make-walkin-appointment-nurse', methods=['PUT'])
+def make_walkin_appointment():
+
+    if request.method == 'PUT':
+
+        #if not cookie_helper.user_is_logged(request):
+        #    return js.create_json(data=None, message="User is not logged", return_code=js.ResponseReturnCode.CODE_400)
+
+        availability_id = int(request.args.get('availability_id'))
+        patient_id = request.args.get('patient_id')
+
+        patient_service.test_and_set_patient_into_cache(patient_id)
+        patient = patient_service.get_patient(patient_id)
+        availability = availability_service.get_availability(availability_id)
+
+        patient.make_walkin_appointment(availability)
+
+        return js.create_json(data=None, message="Successfully added walkin appointment", return_code=js.ResponseReturnCode.CODE_200)
