ad318cfaa16a9e5f1d0aed4a9c47e2c2b38b23a2
Samuel Gosselin <samuelgosselin1@hotmail.com>
2019-03-08 21:04:01 -0500

    [#84] wrote the attach method of the observer pattern

--- uber_sante/miscellaneous/observable.py
+++ uber_sante/miscellaneous/observable.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+# for my tests
+from uber_sante.models.availability import Availability
+
 class Observable(ABC):
 	"""
 		The abstract class that defines the methods for the
@@ -13,33 +16,65 @@ class Observable(ABC):
 
 	@abstractmethod
 	def detach(self, observer):
-		"""Detaches the observer from this Observable"""
+		"""Detaches an observer from this Observable"""
 		pass
 
 	@abstractmethod
 	def notify(self):
-		"""Notifies the observers """
+		"""Notifies the observer(s) """
 		pass
 
 
 class AvailabilityCanceledObservable(Observable):
+	""" Act as a patient notifier on login"""
 
 	def __init__(self):
-		self.observers = set()
+		# dict, 'keys' are patient_id mapping to availability objects
+		# corresponding to the canceled booking
+		self.patient_id_availabilities = {}
 
-	def attach(self, observer_patient_id):
+	# 1. ready to test
+	def attach(self, patient_id_availability_pair):
+		"""
+		Attaches a patient_id_availability_pair to this notifier
+		:param patient_id_availability_pair: { 'patient_id' : availability_obj }
+		:return: void
+		"""
+
+		# getting the patient id from the dictionary
+		key = next(iter(patient_id_availability_pair))
+
+		# verifying if the patient is already set to be notified
+		# of a canceled booking
+		if self.patient_id_availabilities.__contains__(key):
+			if isinstance(self.patient_id_availabilities[key], list):
+				self.patient_id_availabilities[key].append(patient_id_availability_pair[key])
+			else:
+				# the key has only one value so we create a list of the now 2 values
+				availability_list = [self.patient_id_availabilities[key], patient_id_availability_pair[key]]
+				# put the list back in the dict
+				self.patient_id_availabilities[key] = availability_list
+		else:
+			self.patient_id_availabilities.update(patient_id_availability_pair)
+
+
+
+		# in the code
+
+		# get the booking_id on DELETE /booking
+		# from the booking, get the patient and the availability_id
 		pass
 
-	def detach(self, observer):
+	def detach(self, patient_id):
+		self.patient_id_availabilities.pop(patient_id)
 		pass
 
+	# 2
 	def notify(self):
+		# on /login, as soon as patient is set to cache notify all
+		# because the patient that just logged-in and that is set to cache might be an observer
+		# on the response login add the message to tell the user about the canceled booking
+
 		pass
 
-
-if __name__ == "__main__":
-
-	a = AvailabilityCanceledObservable()
-
-	print(a.observers)
 
