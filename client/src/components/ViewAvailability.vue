6a1b82d2d4ca13a93ae9a7abdba9f5b693d4a0e0
William <william.lussier115@hotmail.com>
2019-03-10 15:38:35 -0400

    Edit modal

--- client/src/components/ViewAvailability.vue
+++ client/src/components/ViewAvailability.vue
@@ -12,18 +12,57 @@
         <th>Cancel</th>
 
         <tr class="container availability-item" v-for="(item) in results.data">
+
           <td> {{item['year']}}-{{item['month']}}-{{item['day']}}</td>
           <td> {{item['start']}}</td>
           <td> {{item['room']}}</td>
           <td> {{item['booking_type']}}</td>
           <td>
-            <button :id="item['id']" v-on:click="edit(item['id'])"> edit</button>
+            <button v-b-modal="'modal' + item['id']">Edit</button>
           </td>
           <td>
             <button :id="item['id']" v-on:click="cancel(item['id'])"> Cancel</button>
           </td>
 
+          <!-- Modal Component -->
+          <b-modal :id="'modal' + item['id']" hide-footer centered title="Edit Availability">
+            <form id="form-availability" class="form" @submit.prevent="edit">
+              <h3 class="error-message">{{message}}</h3>
+              <div class="form-group">
+                <label for="booking_type">Availability Type</label>
+                <select v-model="booking_type" name="book_type">
+                  <option value="ANNUAL">Annual</option>
+                  <option value="WALKIN">Walkin</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="start">Appointment Time</label>
+                <input v-if="booking_type === 'WALKIN'" type="time" min="9:00" max="16:00" step="1200"
+                       v-model="start">
+                <input v-else type="time" min="9:00" max="16:00" step="3600" v-model="start">
+                <br>
+                <span class="help-text">Hours are 9am to 4pm</span>
+              </div>
+              <div class="form-group">
+                <label for="room">Room</label>
+                <select v-model="room" name="book_type">
+                  <option value="101">101</option>
+                  <option value="102">102</option>
+                  <option value="103">103</option>
+                  <option value="104">104</option>
+                  <option value="105">105</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="date">Date</label>
+                <input type="Date" class="input" name="date" v-model="date">
+              </div>
+              <button type="submit" class="btn btn-default submit">Update</button>
+            </form>
+          </b-modal>
+
         </tr>
+
       </table>
     </div>
   </div>
@@ -39,7 +78,16 @@
       return {
         doctor_id: this.$cookies.get('id'),
         results: '',
-        id: ''
+        id: '',
+        start: '',
+        room: '',
+        year: '',
+        month: '',
+        day: '',
+        date: '',
+        booking_type: '',
+        newDate: '',
+        message: '',
       };
     },
 
@@ -59,7 +107,24 @@
     },
 
     methods: {
-      edit: function (identifier) {
+      edit: function () {
+        this.newDate = this.date.split('-');
+        axios.put('http://127.0.0.1:5000/availability', {
+          start: this.start,
+          room: this.room,
+          year: this.newDate[0],
+          month: this.newDate[1],
+          day: this.newDate[2],
+          booking_type: this.booking_type,
+          doctor_id: this.doctor_id
+        })
+          .then(function (response) {
+            alert(response.data.message);
+          })
+          .catch(error => {
+            console.log(error)
+            this.message = error.response.data.error.message;
+          })
       },
 
       cancel: function (identifier) {
