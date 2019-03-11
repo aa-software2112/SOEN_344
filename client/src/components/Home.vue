5931c6b6e3eadc5dd4c37c71b3af287ae6e0cad5
Samuel Gosselin <samuelgosselin1@hotmail.com>
2019-03-10 23:57:16 -0400

    [#84] Home vue now to request patient notifications

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,19 +4,45 @@
     <div class='page-main'>
        <h1>
          {{ msg }}
-         </h1>
+       </h1>
+      <p v-for="msg in loginMsgList">{{msg.toString()}}</p>
     </div>
     
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'Home',
   data() {
     return {
-      msg: "Welcome to UberSante"
+      msg: "Welcome to UberSante",
+      loginMsgList: null
     };
+  },
+  methods: {
+    getLoginMessage() {
+      const path = "http://127.0.0.1:5000/my-update";
+      if (this.$cookies.get('logged') == 'True' && this.$cookies.get('user_type') == 'patient'){
+        axios
+        .get(path)
+        .then(res => {
+          this.loginMsgList = res.data;
+        })
+        .catch(error => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+      }
+      else {
+        console.log("no cookie")
+      }
+    }
+  },
+
+  created() {
+    this.getLoginMessage();
   }
 };
 </script>
