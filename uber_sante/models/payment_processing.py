a4fd193ec2bf39325108fb7b3b325c715c7a00b7
aa-software2112 <a_dreoli@encs.concordia.ca>
2019-03-13 17:33:15 -0400

    Added strategy pattern

--- uber_sante/models/payment_processing.py
+++ uber_sante/models/payment_processing.py
@@ -1,57 +1,96 @@
-class ProcessVisa:
-   
-    @staticmethod
-    def process(cc_information):
+from abc import ABC, abstractmethod
+
+class PaymentStrategy(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def make_payment(self, cc_information):
+        """
+        :param cc_information: Credit card information
+        :return: True/False for successful payment
+        """
+
+class VisaPaymentStrategy(PaymentStrategy):
+
+    def __init__(self):
+        PaymentStrategy.__init__(self)
+
+    def make_payment(self, cc_information):
+        print("Stub Method: Paying with Visa")
         return True
 
 
-class ProcessMasterCard:
-    
-    @staticmethod
-    def process(cc_information):
+class MastercardStrategy(PaymentStrategy):
+    def __init__(self):
+        PaymentStrategy.__init__(self)
+
+    def make_payment(self, cc_information):
+        print("Stub Method: Paying with Mastercard")
         return True
 
 
-class ProcessAmericanExpress:
-    
-    @staticmethod
-    def process(cc_information):
+class AmericanExpressStrategy(PaymentStrategy):
+    def __init__(self):
+        PaymentStrategy.__init__(self)
+
+    def make_payment(self, cc_information):
+        print("Stub Method: Paying with American Express")
         return True
 
 
-class ProcessOthers:
-   
-    @staticmethod
-    def process(cc_inforamtion):
-        return True
+class OtherPaymentStrategy(PaymentStrategy):
+    def __init__(self):
+        PaymentStrategy.__init__(self)
 
+    def make_payment(self, cc_information):
+        print("Stub Method: Paying with Other")
+        return True
 
 class ProcessPayment:
 
-    credit_card_nb = ''
-    first_name = ''
-    last_name = ''
-    ccv = ''
-    expiration_date = ''
-
     def __init__(self):
-        return
+        self.strategy = None
+        pass
 
     def checkout(self, credit_card):
 
-        cc_type = credit_card[1]    
-        result = None
-        
-        if cc_type == 3:
-            result = ProcessAmericanExpress.process(credit_card)
+        if not(isinstance(credit_card, str) and len(credit_card)==16):
+            return False
 
+        cc_type = int(credit_card[0])
+
+        if cc_type == 3:
+            self.set_strategy(AmericanExpressStrategy())
         elif cc_type == 4:
-            result = ProcessVisa.process(credit_card)
-        
+            self.set_strategy(VisaPaymentStrategy())
         elif cc_type == 5:
-            result = ProcessMasterCard.process(credit_card)
-        
+            self.set_strategy(MastercardStrategy())
         else:
-            result = ProcessOthers.process(credit_card)
-        
-        return result
+            self.set_strategy(OtherPaymentStrategy())
+
+        return self.make_payment(credit_card)
+
+    def set_strategy(self, strat):
+
+        if isinstance(strat, PaymentStrategy):
+            self.strategy = strat
+
+    def make_payment(self, cc_information):
+
+        if self.strategy is None:
+            return False
+        return self.strategy.make_payment(cc_information)
+
+
+if __name__ == "__main__":
+
+    credit_card_american_express = "3234567890123456"
+    credit_card_visa = "4234567890123456"
+    credit_card_master_card = "5234567890123456"
+    credit_card_other = "6234567890123456"
+    ProcessPayment().checkout(credit_card_american_express)
+    ProcessPayment().checkout(credit_card_visa)
+    ProcessPayment().checkout(credit_card_master_card)
+    ProcessPayment().checkout(credit_card_other)
