ad18e5d150744ace5468d3e952170fe64a367ebb
Krishna Patel <thekrish360@hotmail.com>
2019-03-13 14:01:59 -0400

    [#120] Added Strategy pattern

--- uber_sante/models/payment_processing.py
+++ uber_sante/models/payment_processing.py
@@ -1,3 +1,31 @@
+class ProcessVisa:
+   
+    @staticmethod
+    def process(cc_information):
+        return True
+
+
+class ProcessMasterCard:
+    
+    @staticmethod
+    def process(cc_information):
+        return True
+
+
+class ProcessAmericanExpress:
+    
+    @staticmethod
+    def process(cc_information):
+        return True
+
+
+class ProcessOthers:
+   
+    @staticmethod
+    def process(cc_inforamtion):
+        return True
+
+
 class ProcessPayment:
 
     credit_card_nb = ''
@@ -6,5 +34,24 @@ class ProcessPayment:
     ccv = ''
     expiration_date = ''
 
-    def checkout(self, user_id):
-        return True
+    def __init__(self):
+        return
+
+    def checkout(self, credit_card):
+
+        cc_type = credit_card[1]    
+        result = None
+        
+        if cc_type == 3:
+            result = ProcessAmericanExpress.process(credit_card)
+
+        elif cc_type == 4:
+            result = ProcessVisa.process(credit_card)
+        
+        elif cc_type == 5:
+            result = ProcessMasterCard.process(credit_card)
+        
+        else:
+            result = ProcessOthers.process(credit_card)
+        
+        return result
